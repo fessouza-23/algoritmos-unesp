f44df26efc507d3123b30fa91fae985aaa6d4f3f
fessouza-dev <joao.fessouza@gmail.com>
2023-09-01 01:18:19 -0300

    Update

--- Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
+++ Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
@@ -7,30 +7,35 @@ int main()
     FILE *file, *file2, *file3;
     char texto[100];
 
-    if((file = fopen("arq.txt", "r")) == NULL) {
+    if ((file = fopen("arq.txt", "r")) == NULL)
+    {
         printf("Erro ao abrir o arquivo.\n\n");
         getch();
         return;
     }
 
-    if((file2 = fopen("arq2.txt", "r")) == NULL) {
+    if ((file2 = fopen("arq2.txt", "r")) == NULL)
+    {
         printf("Erro ao abrir o arquivo.\n\n");
         getch();
         return;
     }
 
-    if((file3 = fopen("arq3.txt", "a+")) == NULL) {
+    if ((file3 = fopen("arq3.txt", "a+")) == NULL)
+    {
         printf("Erro ao abrir o arquivo.\n\n");
         getch();
         return;
     }
-    
-    while(fgets(texto, 100, file) != NULL){
+
+    while (fgets(texto, 100, file) != NULL)
+    {
         fprintf(file3, "%s", texto);
         putc('\n', file3);
     }
 
-    while(fgets(texto, 100, file2) != NULL){
+    while (fgets(texto, 100, file2) != NULL)
+    {
         fprintf(file3, "%s", texto);
         putc('\n', file3);
     }
