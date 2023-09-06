b12c76fcaf60ab59167c2669aba186358c32687e
fessouza-dev <joao.fessouza@gmail.com>
2023-09-06 11:54:57 -0300

    Formatando códigos

--- Algoritmos II/Manipulacao de Arquivos - Texto/ex3.c
+++ Algoritmos II/Manipulacao de Arquivos - Texto/ex3.c
@@ -1,61 +1,52 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-int main()
-{
-    FILE *file;
-    char arquivo[20];
-    char caracter;
-    int quant_digitos = 0, quant_letras = 0, quant_linhas = 1;
-    int frequencia[256] = {0};
+int main() {
+  FILE *file;
+  char arquivo[20];
+  char caracter;
+  int quant_digitos = 0, quant_letras = 0, quant_linhas = 1;
+  int frequencia[256] = {0};
 
-    system("cls");
-    printf("Nome do arquivo de texto .txt: ");
-    scanf("%s", arquivo);
+  system("cls");
+  printf("Nome do arquivo de texto .txt: ");
+  scanf("%s", arquivo);
 
-    if ((file = fopen(arquivo, "r")) == NULL)
-    {
-        printf("Erro ao abrir o arquivo.\n\n");
-        return 1;
+  if ((file = fopen(arquivo, "r")) == NULL) {
+    printf("Erro ao abrir o arquivo.\n\n");
+    return 1;
+  }
+
+  while ((caracter = fgetc(file)) != EOF) {
+    if (isalpha(caracter)) {
+      quant_letras++;
+    } else if (isdigit(caracter)) {
+      quant_digitos++;
+    } else if (caracter == '\n') {
+      quant_linhas++;
     }
+    frequencia[(int)caracter]++;
+  }
 
-    while ((caracter = fgetc(file)) != EOF)
-    {
-        if (isalpha(caracter))
-        {
-            quant_letras++;
-        }
-        else if (isdigit(caracter))
-        {
-            quant_digitos++;
-        }
-        else if (caracter == '\n')
-        {
-            quant_linhas++;
-        }
-        frequencia[(int)caracter]++;
+  printf("Quantidade de letras: %d\n", quant_letras);
+  printf("Quantidade de digitos: %d\n", quant_digitos);
+  printf("Quantidade de linhas: %d\n", quant_linhas);
+
+  char caractere_mais_frequente = 0;
+  int max_frequencia = 0;
+
+  for (int i = 0; i < 256; i++) {
+    if (frequencia[i] > max_frequencia) {
+      max_frequencia = frequencia[i];
+      caractere_mais_frequente = (char)i;
     }
+  }
 
-    printf("Quantidade de letras: %d\n", quant_letras);
-    printf("Quantidade de digitos: %d\n", quant_digitos);
-    printf("Quantidade de linhas: %d\n", quant_linhas);
+  printf("Caractere mais frequente: %c (aparece %d vezes)\n",
+         caractere_mais_frequente, max_frequencia);
 
-    char caractere_mais_frequente = 0;
-    int max_frequencia = 0;
-
-    for (int i = 0; i < 256; i++)
-    {
-        if (frequencia[i] > max_frequencia)
-        {
-            max_frequencia = frequencia[i];
-            caractere_mais_frequente = (char)i;
-        }
-    }
-
-    printf("Caractere mais frequente: %c (aparece %d vezes)\n", caractere_mais_frequente, max_frequencia);
-
-    fclose(file);
-    return 0;
+  fclose(file);
+  return 0;
 }
