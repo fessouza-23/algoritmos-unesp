a548fb0c77ef4c4d9ab967697d34b58021fef547
fessouza-dev <joao.fessouza@gmail.com>
2023-09-06 11:55:33 -0300

    Update

--- Algoritmos II/Manipulacao de Arquivos - Texto/ex1.c
+++ Algoritmos II/Manipulacao de Arquivos - Texto/ex1.c
@@ -1,49 +1,46 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-int main()
-{
-    FILE *file;
-    char caractere, arquivo[20];
-    int quant_vogais = 0, quant_consoantes = 0, achou;
-    char vogais[] = {"AEIOU"};
+int main() {
+  FILE *file;
+  char caractere, nome_arquivo[20];
+  int quant_vogais = 0, quant_consoantes = 0, achou;
+  char vogais[] = {"AEIOU"};
 
-    system("cls");
-    printf("Arquivo de texto .txt: ");
-    scanf("%s", arquivo);
+  system("cls");
+  printf("Arquivo de texto .txt: ");
+  scanf("%s", nome_arquivo);
 
-    if ((file = fopen(arquivo, "r")) == NULL)
-    {
-        printf("Erro ao abrir o arquivo.\n\n");
-        getch();
-        return;
-    }
-
-    while (!feof(file))
-    {
-        caractere = getc(file);
-        achou = 0;
-        if (toupper(caractere) >= 'A' && toupper(caractere) <= 'Z')
-        {
-            for (int i = 0; i < strlen(vogais); i++)
-            {
-                if (toupper(caractere) == vogais[i])
-                {
-                    quant_vogais++;
-                    achou = 1;
-                    break;
-                }
-            }
-            if (!achou)
-            {
-                quant_consoantes++;
-            }
-        }
-    }
-
-    printf("Vogais: %d\nConsoantes: %d\n", quant_vogais, quant_consoantes);
+  if ((file = fopen(nome_arquivo, "r")) == NULL) {
+    printf("Erro ao abrir o arquivo.\n\n");
     getch();
-    return 0;
+    return;
+  }
+
+  while (!feof(file)) {
+    caractere = getc(file);
+    achou = 0;
+    if (toupper(caractere) >= 'A' && toupper(caractere) <= 'Z') {
+      for (int i = 0; i < strlen(vogais); i++)
+
+      {
+        if (toupper(caractere) == vogais[i])
+
+        {
+          quant_vogais++;
+          achou = 1;
+          break;
+        }
+      }
+      if (!achou) {
+        quant_consoantes++;
+      }
+    }
+  }
+
+  printf("Vogais: %d\nConsoantes: %d\n", quant_vogais, quant_consoantes);
+  getch();
+  return 0;
 }
--- Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
+++ Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
@@ -2,79 +2,69 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    FILE *file, *file2, *file3;
-    char texto[81], nome_arquivo1[20];
+int main() {
+  FILE *file, *file2, *file3;
+  char texto[81], nome_arquivo1[20];
 
-    system("cls");
-    printf("- Gravando no primeiro arquivo -\n");
-    if ((file = fopen("arquivo1.txt", "a+")) == NULL)
-    {
-        printf("\n file não pode ser aberto");
-        getch();
-        return 1;
-    }
-
-    while (strlen(gets(texto)) > 0)
-    {
-        fputs(texto, file);
-        putc('\n', file);
-    }
-    fclose(file);
-
-    system("cls");
-    printf("- Gravando no segundo arquivo -\n");
-    if ((file = fopen("arquivo2.txt", "a+")) == NULL)
-    {
-        printf("\n file não pode ser aberto");
-        getch();
-        return 1;
-    }
-
-    while (strlen(gets(texto)) > 0)
-    {
-        fputs(texto, file);
-        putc('\n', file);
-    }
-    fclose(file);
-
-    if ((file = fopen("arquivo1.txt", "r")) == NULL)
-    {
-        printf("Erro ao abrir o arquivo1.txt\n");
-        getch();
-        return 1;
-    }
-
-    if ((file2 = fopen("arquivo2.txt", "r")) == NULL)
-    {
-        printf("Erro ao abrir o arquivo2.txt\n");
-        getch();
-        return 1;
-    }
-
-    if ((file3 = fopen("arquivo_merge.txt", "a+")) == NULL)
-    {
-        printf("Erro ao abrir o arquivo_merge.txt\n");
-        getch();
-        return 1;
-    }
-
-    while (fgets(texto, 100, file) != NULL)
-    {
-        fprintf(file3, "%s", texto);
-    }
-
-    while (fgets(texto, 100, file2) != NULL)
-    {
-        fprintf(file3, "%s", texto);
-    }
-
-    printf("Arquivo merge criado.\n");
+  system("cls");
+  printf("- Gravando no primeiro arquivo -\n");
+  if ((file = fopen("arquivo1.txt", "a+")) == NULL) {
+    printf("\n file não pode ser aberto");
     getch();
-    fclose(file);
-    fclose(file2);
-    fclose(file3);
+    return 1;
+  }
 
-    return 0;
+  while (strlen(gets(texto)) > 0) {
+    fputs(texto, file);
+    putc('\n', file);
+  }
+  fclose(file);
+
+  system("cls");
+  printf("- Gravando no segundo arquivo -\n");
+  if ((file = fopen("arquivo2.txt", "a+")) == NULL) {
+    printf("\n file não pode ser aberto");
+    getch();
+    return 1;
+  }
+
+  while (strlen(gets(texto)) > 0) {
+    fputs(texto, file);
+    putc('\n', file);
+  }
+  fclose(file);
+
+  if ((file = fopen("arquivo1.txt", "r")) == NULL) {
+    printf("Erro ao abrir o arquivo1.txt\n");
+    getch();
+    return 1;
+  }
+
+  if ((file2 = fopen("arquivo2.txt", "r")) == NULL) {
+    printf("Erro ao abrir o arquivo2.txt\n");
+    getch();
+    return 1;
+  }
+
+  if ((file3 = fopen("arquivo_merge.txt", "a+")) == NULL) {
+    printf("Erro ao abrir o arquivo_merge.txt\n");
+    getch();
+    return 1;
+  }
+
+  while (fgets(texto, 100, file) != NULL) {
+    fprintf(file3, "%s", texto);
+  }
+
+  while (fgets(texto, 100, file2) != NULL) {
+    fprintf(file3, "%s", texto);
+  }
+
+  printf("Arquivo merge criado.\n");
+  getch();
+  fclose(file);
+  fclose(file2);
+  fclose(file3);
+
+  return 0;
 }
