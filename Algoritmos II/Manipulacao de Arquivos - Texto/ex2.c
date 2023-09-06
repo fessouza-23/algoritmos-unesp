5bdb984ed6f020d46eb79d47de5e3b0fe05efec0
fessouza-dev <joao.fessouza@gmail.com>
2023-09-06 12:35:39 -0300

    Update ex2

--- Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
+++ Algoritmos II/Manipulacao de Arquivos - Texto/ex2.c
@@ -2,56 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+FILE *abrirArquivo(const char nome_arquivo[20], const char op[5]) {
+  FILE *file;
+  if ((file = fopen(nome_arquivo, op)) == NULL) {
+    printf("Erro ao abrir o arquivo.\n\n");
+    exit(1);
+  }
+  return file;
+}
+
 int main() {
   FILE *file, *file2, *file3;
   char texto[81], nome_arquivo1[20];
 
+  // Criando arquivo 1
   system("cls");
   printf("- Gravando no primeiro arquivo -\n");
-  if ((file = fopen("arquivo1.txt", "a+")) == NULL) {
-    printf("\n file não pode ser aberto");
-    getch();
-    return 1;
-  }
+  file = abrirArquivo("arquivo1.txt", "w");
 
+  // Gravando no arquivo 1
   while (strlen(gets(texto)) > 0) {
     fputs(texto, file);
     putc('\n', file);
   }
   fclose(file);
 
+  // Criando arquivo 2
   system("cls");
   printf("- Gravando no segundo arquivo -\n");
-  if ((file = fopen("arquivo2.txt", "a+")) == NULL) {
-    printf("\n file não pode ser aberto");
-    getch();
-    return 1;
-  }
+  file = abrirArquivo("arquivo2.txt", "w");
 
+  // Gravando no arquivo 2
   while (strlen(gets(texto)) > 0) {
     fputs(texto, file);
     putc('\n', file);
   }
   fclose(file);
 
-  if ((file = fopen("arquivo1.txt", "r")) == NULL) {
-    printf("Erro ao abrir o arquivo1.txt\n");
-    getch();
-    return 1;
-  }
+  file = abrirArquivo("arquivo1.txt", "r");
+  file2 = abrirArquivo("arquivo2.txt", "r");
+  file3 = abrirArquivo("arquivo_merge.txt", "w");
 
-  if ((file2 = fopen("arquivo2.txt", "r")) == NULL) {
-    printf("Erro ao abrir o arquivo2.txt\n");
-    getch();
-    return 1;
-  }
-
-  if ((file3 = fopen("arquivo_merge.txt", "a+")) == NULL) {
-    printf("Erro ao abrir o arquivo_merge.txt\n");
-    getch();
-    return 1;
-  }
-
+  // Gravando no arquivo 3
   while (fgets(texto, 100, file) != NULL) {
     fprintf(file3, "%s", texto);
   }
