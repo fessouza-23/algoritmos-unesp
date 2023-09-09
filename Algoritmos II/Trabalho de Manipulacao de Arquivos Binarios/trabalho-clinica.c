f6cc0e6abc30cdb76dfc29d104317f2c8a0a9707
fessouza-dev <joao.fessouza@gmail.com>
2023-09-09 09:50:22 -0300

    Update

--- Algoritmos II/Trabalho de Manipulacao de Arquivos Binarios/trabalho-clinica.c
+++ Algoritmos II/Trabalho de Manipulacao de Arquivos Binarios/trabalho-clinica.c
@@ -49,25 +49,91 @@ void cadastrarCliente(Cliente *c) {
 
 // Esta função não está dentro as necessárias, serve apenas para testar
 void consultarCliente() {
-    FILE *file;
-    Cliente c;
+  FILE *file;
+  Cliente c;
 
-    system("cls");
+  system("cls");
 
-    // Abra o arquivo para leitura
-    file = abrirArquivo("clientes.dat", "rb");
+  // Abra o arquivo para leitura
+  file = abrirArquivo("clientes.dat", "rb");
 
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return;
+  if (file == NULL) {
+    printf("Erro ao abrir o arquivo.\n");
+    return;
+  }
+
+  while (fread(&c, sizeof(Cliente), 1, file) == 1) {
+    printf("%d %s %d %s %s\n", c.codigo, c.nome, c.idade, c.endereco, c.fone);
+  }
+
+  fclose(file);
+  getch();
+}
+
+int confirma() {
+  char resp;
+  do {
+    resp = getch();
+    if (toupper(resp) != 'S' && toupper(resp) != 'N') {
+      printf("Opção invalida.\n");
     }
+  } while (toupper(resp) != 'S' && toupper(resp) != 'N');
+  if (toupper(resp) == 'S') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
-    while (fread(&c, sizeof(Cliente), 1, file) == 1) {
-        printf("%d %s %d %s %s\n", c.codigo, c.nome, c.idade, c.endereco, c.fone);
+void atualizarCliente() {
+  FILE *file, *tempFile;
+  int codigo_ref; // Código de referência = código digitado para procurar o
+                  // cliente
+  Cliente c;
+  int achou = 0; // Variável para verificar se o cliente foi encontrado
+
+  system("cls");
+  printf("Digite o código do cliente: ");
+  scanf("%d", &codigo_ref);
+
+  file = abrirArquivo("clientes.dat", "rb");
+  tempFile = abrirArquivo("temp.dat", "wb"); // Arquivo temporário para escrita
+
+  while (fread(&c, sizeof(Cliente), 1, file) == 1) {
+    if (c.codigo == codigo_ref) {
+      system("cls");
+      printf("Cliente encontrado de código %d\n", codigo_ref);
+      printf("Código: %d\n", c.codigo);
+      printf("Nome: %s\n", c.nome);
+      printf("Idade: %d\n", c.idade);
+      printf("Endereço: %s\n", c.endereco);
+      printf("Telefone: %s\n", c.fone);
+      printf("\nDeseja realmente alterar o cliente? (s/n): ");
+      if (confirma()) {
+        system("cls");
+        fflush(stdin);
+        printf("Novo Nome: ");
+        gets(c.nome);
+        printf("Nova Idade: ");
+        scanf("%d", &c.idade);
+        fflush(stdin);
+        printf("Novo Endereço: ");
+        gets(c.endereco);
+        printf("Novo Telefone: ");
+        gets(c.fone);
+        fseek(file, -sizeof(Cliente), SEEK_CUR);
+        fwrite(&c, sizeof(Cliente), 1, file);
+        achou = 1;
+        fclose(file);
+      }
     }
+  }
 
-    fclose(file);
-    getch();
+  if (!achou) {
+    printf("Cliente não encontrado!\n");
+  } else {
+    printf("Cliente atualizado com sucesso!\n");
+  }
 }
 
 void menu(Cliente *c) {
@@ -79,6 +145,7 @@ void menu(Cliente *c) {
            "=======================================\n");
     printf("1 - Cadastrar cliente\n");
     printf("2 - Listar clientes\n");
+    printf("3- Atualizar cliente\n");
     printf("ESC - Sair\n");
     op = getch();
 
@@ -88,7 +155,11 @@ void menu(Cliente *c) {
       break;
 
     case '2':
-      consultarCliente(c);
+      consultarCliente();
+      break;
+
+    case '3':
+      atualizarCliente(c);
       break;
 
     case esc:
